7d2c7bda417bbeed209b98bcd67609dda6c36a72
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-09-23 21:35:14 -0400

    latency too large, flutter is not a good candidate. perhaps a webview in the future

---
+++ bento_arena/lib/controller.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ControllerPage extends StatefulWidget {
+  const ControllerPage({super.key});
+
+  @override
+  State<ControllerPage> createState() => _ControllerPageState();
+}
+
+class _ControllerPageState extends State<ControllerPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () async {
+                await sendRequestToFlaskApp('http://20.98.164.88:3389/run');
+              },
+              icon: Icon(Icons.arrow_upward),
+            ),
+            IconButton(
+              onPressed: () async {
+                await sendRequestToFlaskApp('http://20.98.164.88:3389/stop');
+              },
+              icon: Icon(Icons.arrow_downward),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Future<void> sendCurlRequest(String url_str) async {
+  final url = Uri.parse(url_str);
+
+  final response = await http.post(url);
+
+  /*if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    throw Exception('Failed to send request: ${response.statusCode}');
+  }*/
+}
+
+Future<void> sendRequestToFlaskApp(String str_url) async {
+  final url = Uri.parse(str_url);
+
+  try {
+    final response = await http.get(url).timeout(Duration(seconds: 10));
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      // Request successful
+      print('Request successful');
+      // Process the response here if needed
+    } else {
+      // Request failed
+      print('Request failed with status: ${response.statusCode}');
+    }
+  } catch (e) {
+    // Handle any errors
+    print('Error occurred: $e');
+  }
+}
