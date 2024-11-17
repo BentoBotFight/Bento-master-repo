3059db546fc1db342ebf23533a3508acc340c723
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-11-16 22:31:42 -0500

    updated stream site

--- bento_arena/lib/components/live_stream.dart
+++ bento_arena/lib/components/live_stream.dart
@@ -26,7 +26,8 @@ class LiveStream extends StatelessWidget {
                     Webview(url:
                     //'https://p2p.mirotalk.com/join/bento'
                       //'https://stream.bentobot.tech/viewer?id=bento&name=$email'
-                      "https://viewer.millicast.com?streamId=4zxunL/myStreamName"
+                      'https://viewer.millicast.com?streamId=W3yfVd/Bento%20Stream', //tungvung
+                      //"https://viewer.millicast.com?streamId=4zxunL/myStreamName"
                   ),
                     Container(color: AppColors.comfyGreen, padding: EdgeInsets.all(8) ,child: Text('controlling $bento_name')),
                   ])),
